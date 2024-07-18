b9d546833cc7045b86949d47a5304260477269ee
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-07-18 18:09:28 +0200

    Feat: AppTypology

---
+++ src/components/AppTypology.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    typologyObject: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="p-1 d-flex justify-content-center ">
+    <button class="btn btn-warning custom-button-typology text-white">
+      <img src="" class="" alt="" />
+      <span class="">{{ typologyObject.name }}</span>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.custom-button-typology  {
+  width: 150px;
+  height: 50px; 
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,95 +2,110 @@
 import axios from "axios";
 import { store } from "../store.js";
 import AppCard from "../components/AppCard.vue";
+import AppTypology from "../components/AppTypology.vue";
 
 export default {
-    components: {
-        AppCard,
+  components: {
+    AppCard,
+    AppTypology,
+  },
+  data() {
+    return {
+      filters: [],
+      query: "",
+      store,
+      typology_id: "",
+      isTypology: false,
+    };
+  },
+  methods: {
+    getFilters() {
+      axios.get("http://localhost:8000/api/typologies/").then((resp) => {
+        console.log(resp.data.results);
+        this.filters = resp.data.results;
+        this.store.typologiesArray = resp.data.results;
+      });
     },
-    data() {
-        return {
-            filters: [],
-            query: "",
-            store,
-            typology_id: "",
-            isTypology: false,
-        };
+    getRestaurants() {
+      axios
+        .get("http://localhost:8000/api/restaurants/", {
+          params: { typology_id: this.typology_id, search: store.searchQuery },
+        })
+        .then((resp) => {
+          // console.log(resp.data.results.data);
+          this.store.restaurantsArray = resp.data.results;
+        });
     },
-    methods: {
-        getFilters() {
-            axios.get("http://localhost:8000/api/typologies/").then((resp) => {
-                console.log(resp.data.results);
-                this.filters = resp.data.results;
-            });
-        },
-        getRestaurants() {
-            axios.get("http://localhost:8000/api/restaurants/", { params: { typology_id: this.typology_id, search: store.searchQuery } }).then((resp) => {
-                // console.log(resp.data.results.data);
-                this.store.restaurantsArray = resp.data.results;
-            });
-        },
-        searchAction() {
-            if (this.searchQuery == '') {
-                this.getRestaurants();
-            } else {
-                this.store.searchQuery = this.query;
-                this.getRestaurants();
-            }
-        },
-
-        filterRestaurants(id) {
-            if (this.isTypology == false && this.typology_id !== id) {
-                this.typology_id = id;
-                this.isTypology = true;
-            } else if (this.isTypology == true && this.typology_id === id) {
-                this.typology_id = '';
-            } else if (this.isTypology == true && this.typology_id !== id) {
-                this.typology_id = id;
-            }
-
-            console.log(this.typology_id);
-            console.log(this.isTypology);
-            this.getRestaurants();
-        }
-    },
-    created() {
-        this.getFilters();
+    searchAction() {
+      if (this.searchQuery == "") {
         this.getRestaurants();
-    }
+      } else {
+        this.store.searchQuery = this.query;
+        this.getRestaurants();
+      }
+    },
+
+    filterRestaurants(id) {
+      if (this.isTypology == false && this.typology_id !== id) {
+        this.typology_id = id;
+        this.isTypology = true;
+      } else if (this.isTypology == true && this.typology_id === id) {
+        this.typology_id = "";
+        this.isTypology = false;
+      } else if (this.isTypology == true && this.typology_id !== id) {
+        this.typology_id = id;
+      }
+
+      console.log(this.typology_id);
+      console.log(this.isTypology);
+      this.getRestaurants();
+    },
+  },
+  created() {
+    this.getFilters();
+    this.getRestaurants();
+  },
 };
 </script>
 
 <template>
-    <div>
-        <div class="container">
-            <h4>Scegli i tuoi piatti preferiti:</h4>
-            <div class="content">
-                <div class="row">
-                    <div v-for="typology in filters" class="card col-3">
-                        <div class="card-body" @click="filterRestaurants(typology.id)">
+  <div class="container">
+    <h4>Scegli i tuoi piatti preferiti:</h4>
+    <div class="row">
+      <div v-for="typObj in filters" class="col-3">
+        <AppTypology
+          :typologyObject="typObj"
+          @click="filterRestaurants(typObj.id)"
+        />
+        <!-- <div class="card-body" @click="filterRestaurants(typology.id)">
                             <span>{{ typology.name }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="pt-5">
-                <label for="search">Ricerca</label>
-                <input type="text" id="search" name="search" @keyup.enter="searchAction()" v-model="query" />
-                <button @click="searchAction()">Cerca</button>
-            </div>
-
-            <div>
-                <div class="content">
-                    <div class="row">
-                        <div class="col-4" v-for="restObj in store.restaurantsArray">
-                            <AppCard :restaurantObject="restObj" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </div> -->
+      </div>
     </div>
+  </div>
+  <hr />
+
+  <div class="container">
+    <label for="search">Ricerca</label>
+    <input
+      type="text"
+      id="search"
+      name="search"
+      @keyup.enter="searchAction()"
+      v-model="query"
+    />
+    <button @click="searchAction()">Cerca</button>
+  </div>
+  <hr />
+
+  <div class="container">
+    <h4>I ristoranti:</h4>
+    <div class="row">
+      <div class="col-4" v-for="restObj in store.restaurantsArray">
+        <AppCard :restaurantObject="restObj" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss"></style>
