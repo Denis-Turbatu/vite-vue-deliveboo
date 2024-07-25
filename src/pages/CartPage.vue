837e48dd652a7c3111a385be1e8a14495a92f1a6
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-07-25 17:54:56 +0200

    Feat: fix CarPage with responsive style

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -84,7 +84,7 @@ export default {
 <template>
   <div v-if="this.store.cardNum !== 0" class="container">
     <div class="cart my-4">
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between flex-wrap">
         <h1 class="mb-4">Il tuo carrello</h1>
         <router-link :to="`/ristorante/${slug}`">
           <button class="btn text-warning">
@@ -95,56 +95,28 @@ export default {
       </div>
       <div class="row">
         <div class="col-12">
-          <div
-            class="card mb-4 p-3 d-flex flex-row align-items-center justify-content-between"
-          >
-            <div class="flex-grow-1 d-flex flex-column">
+          <div class="card mb-4 p-3">
+            <div class="flex-grow-1">
               <div
-                class="info border border-1 my-2 w-75 rounded me-3 d-flex align-items-center"
+                class="info border border-1 my-2 w-100 rounded p-3 d-flex flex-column flex-md-row align-items-center"
                 v-for="(curCartProd, index) in cartArray"
                 :key="index"
               >
-                <div class="ms-3 me-5">
-                  <span>{{
-                    getProductPrice(curCartProd.price, curCartProd.quantity)
-                  }}</span>
+                <div class="d-flex flex-column align-items-center align-items-md-start ms-md-3 me-md-5 mb-3 mb-md-0">
+                  <span>{{ getProductPrice(curCartProd.price, curCartProd.quantity) }} €</span>
                 </div>
-                <div
-                  class="ms-1 d-flex align-items-center justify-content-between flex-grow-1"
-                >
-                  <section class="ms-2">
-                    <h6 class="fw-semibold mt-2 mb-2">
-                      {{ curCartProd.name }}
-                    </h6>
+                <div class="flex-grow-1 d-flex flex-column flex-md-row align-items-center justify-content-between">
+                  <section class="ms-2 text-center text-md-start">
+                    <h6 class="fw-semibold mt-2 mb-2">{{ curCartProd.name }}</h6>
                     <p class="m-0 mb-1">{{ curCartProd.description }}</p>
                   </section>
-
-                  <!-- INPUT -->
-                  <div class="me-3">
-                    <div
-                      class="input-group input-group-sm ms-auto"
-                      style="width: 100px"
-                    >
-                      <button
-                        class="btn btn-outline-warning btn-sm"
-                        type="button"  @click.prevent="decrementCart(index)"
-                      >
-                        -
-                      </button>
-                      <input
-                        type="text"
-                        class="form-control text-center"
-                        v-model.number="curCartProd.quantity"
-                        readonly
-                      />
-                      <button
-                        class="btn btn-outline-warning btn-sm"
-                        type="button"  @click.prevent="incrementCart(index)"
-                      >
-                        +
-                      </button>
+                  <div class="me-3 d-flex flex-column align-items-center flex-md-row">
+                    <div class="input-group input-group-sm ms-auto" style="width: 100px">
+                      <button class="btn btn-outline-warning btn-sm" type="button" @click.prevent="decrementCart(index)">-</button>
+                      <input type="text" class="form-control text-center" v-model.number="curCartProd.quantity" readonly />
+                      <button class="btn btn-outline-warning btn-sm" type="button" @click.prevent="incrementCart(index)">+</button>
                     </div>
-                    <span class="me-2" @click="deleteProd(index)">
+                    <span class="me-2 mt-2 mt-md-0 ms-md-2" @click="deleteProd(index)">
                       <i class="fa-solid fa-trash-can text-danger"></i>
                     </span>
                   </div>
@@ -153,17 +125,10 @@ export default {
             </div>
           </div>
         </div>
-        <div class="col-12 d-flex justify-content-between">
-          <router-link
-            :to="{ name: 'spedizione' }"
-            class="btn btn-success ms-3"
-            @click="saveProductsData()"
-            >Procedi all'ordine</router-link
-          >
-
-          <h3>
-            Totale: <span>{{ getOrderPrice() }}</span
-            >€
+        <div class="col-12 d-flex justify-content-between flex-wrap">
+          <router-link :to="{ name: 'spedizione' }" class="btn btn-success ms-3 mb-2" @click="saveProductsData()">Procedi all'ordine</router-link>
+          <h3 class="mb-2">
+            Totale: <span>{{ getOrderPrice() }}</span>€
           </h3>
         </div>
       </div>
@@ -171,13 +136,9 @@ export default {
   </div>
   <div v-else>
     <div class="container">
-      <h1 class="mb-4">
-        Oops! Il tuo carrello è vuoto, ma il tuo stomaco non deve esserlo!
-      </h1>
+      <h1 class="mb-4">Oops! Il tuo carrello è vuoto, ma il tuo stomaco non deve esserlo!</h1>
       <span>Riempi il carrello con amore!</span>
-      <router-link :to="{ name: 'home' }" class="btn btn-success ms-3"
-        >Home</router-link
-      >
+      <router-link :to="{ name: 'home' }" class="btn btn-success ms-3">Home</router-link>
     </div>
   </div>
 </template>
@@ -189,7 +150,23 @@ export default {
   margin-inline: 5px;
   width: 70px;
 }
-.btn-outline-warning:hover{
-  color:white
+.btn-outline-warning:hover {
+  color: white;
+}
+@media (max-width: 768px) {
+  .info {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .input-group {
+    width: 100%;
+    margin-top: 10px;
+  }
+}
+@media (min-width: 769px) {
+   .input-group {
+    margin-right: 10px;
+  }
 }
 </style>
+
