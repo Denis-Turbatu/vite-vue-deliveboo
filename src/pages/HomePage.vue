777851b7ea7093e24c8258742f1dc27377419a3f
renatoirina <irinarenato793@gmail.com>
2024-07-27 16:02:44 +0200

    fix: fixed the responsive for Hero-text and formatted the HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,84 +1,82 @@
 <script>
-  import axios from "axios";
-  import { store } from "../store.js";
-  import AppCard from "../components/AppCard.vue";
-  import AppTypology from "../components/AppTypology.vue";
+import axios from "axios";
+import { store } from "../store.js";
+import AppCard from "../components/AppCard.vue";
+import AppTypology from "../components/AppTypology.vue";
 
-  export default {
-    components: {
-      AppCard,
-      AppTypology,
+export default {
+  components: {
+    AppCard,
+    AppTypology,
+  },
+  data() {
+    return {
+      filters: [],
+      query: "",
+      store,
+      typology_id: "",
+      isLoaded: false,
+    };
+  },
+  methods: {
+    getFilters() {
+      axios.get("http://localhost:8000/api/typologies/").then((resp) => {
+        this.filters = resp.data.results;
+        this.store.typologiesArray = resp.data.results;
+      });
     },
-    data() {
-      return {
-        filters: [],
-        query: "",
-        store,
-        typology_id: "",
-        isLoaded: false,
-      };
-    },
-    methods: {
-      getFilters() {
-        axios.get("http://localhost:8000/api/typologies/").then((resp) => {
-          this.filters = resp.data.results;
-          this.store.typologiesArray = resp.data.results;
+    getRestaurants() {
+      this.isLoaded = true;
+      axios
+        .get("http://localhost:8000/api/restaurants/", {
+          params: { typology_id: this.typology_id, search: store.searchQuery },
+        })
+        .then((resp) => {
+          this.store.restaurantsArray = resp.data.results.sort((a, b) =>
+            a.business_name.localeCompare(b.business_name)
+          );
+          this.isLoaded = false;
         });
-      },
-      getRestaurants() {
-        this.isLoaded = true;
-        axios
-          .get("http://localhost:8000/api/restaurants/", {
-            params: { typology_id: this.typology_id, search: store.searchQuery },
-          })
-          .then((resp) => {
-            this.store.restaurantsArray = resp.data.results.sort((a, b) =>
-              a.business_name.localeCompare(b.business_name)
-            );
-            this.isLoaded = false;
-          });
-      },
-      searchAction() {
-        if (this.searchQuery == "") {
-          this.getRestaurants();
-        } else {
-          this.store.searchQuery = this.query;
-          this.getRestaurants();
-        }
-      },
-      filterRestaurants(id) {
-        if (this.typology_id === id) {
-          this.typology_id = "";
-        } else {
-          this.typology_id = id;
-        }
+    },
+    searchAction() {
+      if (this.searchQuery == "") {
         this.getRestaurants();
-      },
-      isActive(id) {
-        return this.typology_id === id;
-      },
-      getImageUrl(name, ext) {
-        return new URL(`../assets/img/${name}.${ext}`, import.meta.url).href
+      } else {
+        this.store.searchQuery = this.query;
+        this.getRestaurants();
       }
     },
-    created() {
-      this.getFilters();
+    filterRestaurants(id) {
+      if (this.typology_id === id) {
+        this.typology_id = "";
+      } else {
+        this.typology_id = id;
+      }
       this.getRestaurants();
-      this.store.cardNum = JSON.parse(localStorage.getItem("cardNumber")) || 0;
     },
-  };
+    isActive(id) {
+      return this.typology_id === id;
+    },
+    getImageUrl(name, ext) {
+      return new URL(`../assets/img/${name}.${ext}`, import.meta.url).href
+    }
+  },
+  created() {
+    this.getFilters();
+    this.getRestaurants();
+    this.store.cardNum = JSON.parse(localStorage.getItem("cardNumber")) || 0;
+  },
+};
 </script>
 
 <template>
   <main>
     <!-- HERO SECTION -->
-    <div 
-      class="hero-image-container" 
-      :style="{
-        'background' : 'url('+ getImageUrl('pizza','jpg') +')', 
-        'background-size': 'cover',
-        'background-position': 'bottom'
-      }">
+    <div class="hero-image-container" :style="{
+      'background': 'url(' + getImageUrl('pizza', 'jpg') + ')',
+      'background-size': 'cover',
+      'background-position': 'bottom'
+    }">
       <div class="hero-text text-white">
         <h3>SCEGLI I TUOI PIATTI PREFERITI &hearts;</h3>
         <span>Tu ordini noi arriviamo in un DeliveBoo!</span>
@@ -124,41 +122,41 @@
 </template>
 
 <style lang="scss">
-
-main{
+main {
   overflow-y: hidden;
 }
-  .typology-text {
-    z-index: 9999;
-    background-color: white;
-    color: black;
-  }
 
-  .bg-typology {
-    position: relative;
-    background-color: rgba(242, 242, 242, 0.703);
-    background-image: url("https://i.pinimg.com/564x/d3/7c/67/d37c672cdcfeee231731fc9568aa7e8a.jpg");
-    /* // background-size: cover;
+.typology-text {
+  z-index: 9999;
+  background-color: white;
+  color: black;
+}
+
+.bg-typology {
+  position: relative;
+  background-color: rgba(242, 242, 242, 0.703);
+  background-image: url("https://i.pinimg.com/564x/d3/7c/67/d37c672cdcfeee231731fc9568aa7e8a.jpg");
+  /* // background-size: cover;
     // background-position: center; */
-  }
+}
 
-  .bg-typology::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(255,
-        255,
-        255,
-        0.8);
-    /* This adds a white overlay with 50% opacity */
-    pointer-events: none;
-    /* Ensures the overlay doesn't interfere with any interactive elements */
-  }
+.bg-typology::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255,
+      255,
+      255,
+      0.8);
+  /* This adds a white overlay with 50% opacity */
+  pointer-events: none;
+  /* Ensures the overlay doesn't interfere with any interactive elements */
+}
 
-  .hero-image-container {
+.hero-image-container {
   position: relative;
   width: 100%;
   height: 60vh;
@@ -168,24 +166,21 @@ main{
   flex-direction: column;
   justify-content: space-between;
 
-  /* Ensure the image is responsive */
   @media (max-width: 950px) {
     height: 40vh;
     background-position: center;
-
-    .hero-text{
-      font-size: 1.25rem;
-    }
   }
 
   @media (max-width: 480px) {
     height: 30vh;
     background-position: top;
-    
-    .hero-text{
-      font-size: 1rem;
-    }
   }
+
+  @media (max-width: 360px) {
+    height: 30vh;
+    background-position: top;
+  }
+
 }
 
 .hero-text {
@@ -198,6 +193,42 @@ main{
   color: white;
   text-align: right;
   font-size: 1.5rem;
+
+  @media (max-width: 950px) {
+    h3 {
+      font-size: 1.5rem;
+    }
+
+    span {
+      font-size: 1.25rem;
+    }
+  }
+
+  @media (max-width: 650px) {
+    white-space: nowrap;
+  }
+
+  @media (max-width: 480px) {
+    h3 {
+      font-size: 1.25rem;
+    }
+
+    span {
+      font-size: 1rem;
+    }
+  }
+
+  @media (max-width: 360px) {
+    white-space: nowrap;
+
+    h3 {
+      font-size: 1rem;
+    }
+
+    span {
+      font-size: 0.75rem;
+    }
+  }
 }
 
 .search-container {
@@ -215,5 +246,4 @@ main{
   border-radius: 5px;
   box-sizing: border-box;
 }
-
 </style>
