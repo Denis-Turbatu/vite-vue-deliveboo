367a4afbac7552b569e27a46e624e85be7510242
matteocenci <matteocenci01@gmail.com>
2024-07-30 11:23:11 +0200

    feat: add clear local storage

--- src/pages/ThankYouPage.vue
+++ src/pages/ThankYouPage.vue
@@ -101,7 +101,7 @@ export default {
         </div>
         <div class="col-12 d-flex justify-content-between flex-wrap">
           <router-link :to="{ name: 'home' }" class="btn btn-success ms-3 mb-2 mt-2"
-            @click="saveProductsData()">Torna alla Home</router-link>
+            @click="localStorage.clear()">Torna alla Home</router-link>
           <h3 class="mb-2 mt-2">
             Totale: <span>{{ getOrderPrice() }}</span>€
           </h3>
